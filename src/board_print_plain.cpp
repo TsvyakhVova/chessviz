743c017c54750220c23769e34db3b30728b0ae0c
TsvyakhVova <tsvyakh.vova@mail.ru>
2019-05-19 00:20:51 +0700

    Draw a field

---
+++ src/board_print_plain.cpp
@@ -0,0 +1,9 @@
+void ChessField(char field[][9])
+{
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cout << setw(2) << field[i][j];
+        }
+        cout << endl;
+    }
+}
